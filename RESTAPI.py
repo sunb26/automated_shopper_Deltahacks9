5f535ca14d1b96b0402c9ef756eca5b296c28f17
Ben Sun <sunb26@mcmaster.ca>
2023-01-14 23:37:42 -0500

    "added product records + setup polling of prices"

--- RESTAPI.py
+++ RESTAPI.py
@@ -1,24 +1,56 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
+import json
+import pyshorteners
 
 app = Flask(__name__)
 
-account_sid = ""
-auth_token = ""
+account_sid = "ACf8a80465852a8535b32186898182541c"
+auth_token = "c08cf43facecf3900d5eab9ec694bbe9"
 client = Client(account_sid, auth_token)
+productRecords = {}
 
+def shortenURL(url):
+    
+    #Replace url already in records
+
+    #TinyURL shortener service
+    type_tiny = pyshorteners.Shortener()
+    short_url = type_tiny.tinyurl.short(url)
+    
+    return short_url
 
 @app.route("/registeralert", methods=['POST'])
 def register_alert():
-    name = request.form["From"]
-    message = client.messages.create(
-                              body=f'Hi {name}! You have created an alert for',
-                              from_='',
-                              to=''
-                          )
+    name = request.data
+    res_dict = json.loads(name.decode('utf-8'))
+    name = res_dict["product-name"]
+    site = res_dict["website-name"]
+    link = shortenURL(res_dict["item-link"])
+
+
+    strike_price = res_dict["buy-price"]
+    productRecords[link] = {"platform": site, "name": name, "price": strike_price}
+
+    # Serializing json
+    json_object = json.dumps(productRecords, indent=4)
     
-    return Response()
+    # Writing to sample.json
+    with open("productRecords.json", "w") as outfile:
+        outfile.write(json_object)
+
+    message = client.messages.create(
+                              body=f"""Hi! I'm Shop Buddy! You have just set an alert for {name} 
+                              \nLink: {link}
+                              \nDesired Price: {strike_price}
+                              """,
+                              from_='+13854816902',
+                              to='+12894393109'
+    )
+
+    print(productRecords)
+    return Response(status=200, mimetype="text/xml")
 
 @app.route("/sms", methods=['POST'])
 def sms_replies():
@@ -35,6 +67,10 @@ def sms_replies():
     msg = response.message(number)
     return Response(str(response), mimetype="text/xml")
 
+@app.route("/history", methods=["GET"])
+def getHistory():
+    test_data = {"data": "HEllo!"}
+    return jsonify(test_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- webparser.py
+++ webparser.py
@@ -1,21 +1,45 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from twilio.rest import Client
+import time
+import json
 
-driver = webdriver.Chrome(r"C:\Users\bsun7\OneDrive\Documents\chromedriver\chromedriver.exe")
 
-def extract_walmart_price(url):
+
+def extract_walmart_price(driver, url):
     driver.get(url)
     cost = driver.find_element(By.XPATH, r'//*[@id="main-buybox"]/div[1]/div[4]/div/div[1]/div/div/div/span/span').text
 
     return cost
 
-def extract_amazon_price(url):
+def extract_amazon_price(driver, url):
 
     driver.get(url)
     cost = driver.find_element(By.CLASS_NAME, "a-price-whole").text + "." + driver.find_element(By.CLASS_NAME, "a-price-fraction").text
 
     return cost
+
+def notification(cost, name, platform):
+    account_sid = "ACf8a80465852a8535b32186898182541c"
+    auth_token = "c08cf43facecf3900d5eab9ec694bbe9"
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+                              body=f""" Hey Good News! {name} has dropped to {cost} on {platform}!
+                              Go Get It Now or reply to this text with "BUY" to automatically purchase it!
+                              """,
+                              from_='+13854816902',
+                              to='+12894393109'
+    )
+
+def writeJSON(productRecords):
+    # Serializing json
+    json_object = json.dumps(productRecords, indent=4)
+    
+    # Writing to sample.json
+    with open("productRecords.json", "w") as outfile:
+        outfile.write(json_object)
 
 def main():
     """This function will accept the search term and the maximum price you are
@@ -23,15 +47,36 @@ def main():
 
     # startup the webdriver
 
-    options=Options()
-    
-    options.headless = True #choose if we want the web browser to be open when doing the crawling 
+    while True:
+
+        try:
+            
+            with open('productRecords.json') as db:
+                productRecords = json.load(db)
+                print("User Entries: ", productRecords)
+
+        except:
+            continue
+
+        if productRecords:
+            driver = webdriver.Chrome(r"C:\Users\bsun7\OneDrive\Documents\chromedriver\chromedriver.exe")
+            for link in productRecords.keys():
+                platform = productRecords[link]["platform"]
+                price = productRecords[link]["price"]
+                name = productRecords[link]["name"]
+                
+                if platform == "Amazon":
+                    cost = extract_amazon_price(driver, link)
+                    if cost <= price:
+                        notification(cost, name, platform)
+                        productRecords.pop(link)
+                        writeJSON(productRecords)
+
+            driver.close()
+            time.sleep(10)
 
 
 
-    cost = extract_walmart_price("https://www.walmart.ca/en/ip/pediasure-complete-nutritional-supplement-4-x-235-ml-vanilla-kids-nutritional-shake-containing-dha-and-vitamins-helps-promote-weight-gains-when-taken-/6000017348400")
-
-    print(float(cost[1:]))
 
 if __name__ == '__main__':
     main()
