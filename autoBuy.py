be5970cf5485c48b0d5ef4dce74803d8d44af700
Ben Sun <sunb26@mcmaster.ca>
2023-01-15 06:54:14 -0500

    blockchain implementation changes

--- autoBuy.py
+++ autoBuy.py
@@ -6,6 +6,7 @@ import os
 import time
 import random
 
+
 load_dotenv()
 password = os.environ["PASSWORD"]
 
@@ -25,14 +26,10 @@ password = os.environ["PASSWORD"]
 #     time.sleep(50)
 
 def Mockbuy(name, url):
-    driver = webdriver.Chrome(r"C:\Users\bsun7\OneDrive\Documents\chromedriver\chromedriver.exe")
-    driver.get(url)
-    pause = random.randint(5, 10)
-    time.sleep(pause)
-    driver.find_element(By.XPATH, '//*[@id="site-content"]/div/div/div[2]/div[2]/div[2]/div/div/div[2]/div/div[3]/div[3]/button').click()
-    time.sleep(50)
-    blockchain.add_new_transaction(["purchased {name}"])
-    blockchain.mine()
+    transaction = f"purchased {name}"
+    blockchain.create_block_from_transaction([transaction])
 
 #buyAmazon("https://www.amazon.ca/Squishmallow-Stuffed-Animal-Cuddle-Pillow/dp/B09QZ4X7ZV/ref=sr_1_1_sspa?crid=1FYB8GXU69HJ5&keywords=fox+squishmallow&qid=1673772947&sprefix=fox++sq%2Caps%2C87&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyS1BUUjhIODEySzlLJmVuY3J5cHRlZElkPUEwMzE0MzgyMlRMTlU5VjVZTEM5UyZlbmNyeXB0ZWRBZElkPUEwMDk3MTk0MTI3QVVYVk1XT1lUSCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=")
 #buyNoFrills("https://www.nofrills.ca/scented-jasmine-rice/p/20121871_EA")           
+
+Mockbuy("Lego", "http://127.0.0.1:5500/Website/lego-website.html")
